9eefdb0f89e7bd97cdcc8e8162df421c8e81c508
v0agent <it+v0agent@vercel.com>
2026-06-01 11:25:54 +0000

    style: remove brightness and invert classes from image

--- components/home/placement-snapshot.tsx
+++ components/home/placement-snapshot.tsx
@@ -127,7 +127,7 @@ export function PlacementSnapshot() {
                     alt={partner.name}
                     width={100}
                     height={40}
-                    className="object-contain brightness-0 invert"
+                    className="object-contain"
                   />
                 </div>
               ))}
